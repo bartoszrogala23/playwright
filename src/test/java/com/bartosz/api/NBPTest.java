d29910ee41cef224b87e3cf493e3139fa2038c9e
Bartosz Rogala <bartoszrogala23@gmail.com>
2023-11-14 21:35:04 +0100

    refactor api test

--- src/test/java/com/bartosz/api/NBPTest.java
+++ src/test/java/com/bartosz/api/NBPTest.java
@@ -5,17 +5,23 @@ import com.microsoft.playwright.APIRequestContext;
 import com.microsoft.playwright.APIResponse;
 import com.microsoft.playwright.Playwright;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class NBPTest {
 
     @Test
-    public void test() {
+    @DisplayName("Sample GET test")
+    public void getTable() {
         Playwright playwright = Playwright.create();
         APIRequest request = playwright.request();
         APIRequestContext requestContext = request.newContext();
 
         APIResponse response = requestContext.get("http://api.nbp.pl/api/exchangerates/tables/A/");
+        assertThat(response.status()).isEqualTo(SC_OK);
 
     }
 
